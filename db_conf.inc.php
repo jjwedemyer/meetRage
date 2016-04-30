f7dcda7b07ff363f50fb3b2328abb79eb84f9ae6
jjwedemyer <mail@jakob-wedemyer.de>
2016-04-30 22:40:30 +0200

    conf update

--- db_conf.inc.php
+++ db_conf.inc.php
@@ -3,6 +3,8 @@ define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'username');    // DB username
 define('DB_PASSWORD', 'password');    // DB password
 define('DB_DATABASE', 'database');      // DB name
+define('FB_APP_ID','');
+define('FB_APP_SECRET', '');
 $connection = mysql_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD) or die( "Unable to connect");
 $database = mysql_select_db(DB_DATABASE) or die( "Unable to select database");
 ?>
